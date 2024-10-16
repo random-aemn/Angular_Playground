0a5c95ced1b69e00900d2977e3783841c52d9b3c
ChristopherColclough <ccolclough@rbr-tech.com>
2024-10-16 18:11:48 -0400

    Additional exercises for front-end

---
+++ frontend/src/app/fake-holy-grail/fake-holy-grail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fake-holy-grail',
+  templateUrl: './fake-holy-grail.component.html',
+  styleUrls: ['./fake-holy-grail.component.scss']
+})
+export class FakeHolyGrailComponent {
+
+}
---
+++ frontend/src/app/real-holy-grail/real-holy-grail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-real-holy-grail',
+  templateUrl: './real-holy-grail.component.html',
+  styleUrls: ['./real-holy-grail.component.scss']
+})
+export class RealHolyGrailComponent {
+
+}
